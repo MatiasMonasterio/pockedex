b9770153275311d5d0062da85debf880b765d9c9
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-21 12:24:01 -0300

    feat: 🎸 add badge component

---
+++ src/components/Badge/Badge.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="badge">
+    <slot />
+  </span>
+</template>
+
+<style scoped>
+.badge {
+  background-color: rgba(0, 0, 0, 0.08);
+  padding: 0.1rem 0.9rem;
+  border-radius: 100px;
+  text-transform: capitalize;
+  font-size: 0.8rem;
+  font-weight: 600;
+  display: inline-block;
+}
+</style>

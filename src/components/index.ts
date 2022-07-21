081c21f60e7adc91ca186c8e0b0da37f18e7e363
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-21 12:25:26 -0300

    feat: 🎸 add moves table component

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Badget } from "./Badge";
+export { default as Stat } from "./Stat";
+
+export { default as PokemonCard } from "./PokemonCard";
+export { default as MoveTable } from "./MovesTable";

d8e1b2c13082d9f1d679b3909bc534955b6291ae
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-21 12:19:30 -0300

    feat: 🎸 add services with pockedex api

---
+++ src/services/index.ts
@@ -0,0 +1,30 @@
+import type { Pokedex, Pokemon, MoveResponse } from "@/types";
+import type { QueryParams } from "@/types/services";
+
+import { POKEAPI_URL } from "@/config/constants";
+
+export const getPokemons = async ({ offset, limit }: QueryParams): Promise<Pokemon[]> => {
+  const endpoint = `/pokemon?offset=${offset}limit=${limit}`;
+
+  const resp = await fetch(`${POKEAPI_URL}${endpoint}`);
+  if (resp.status !== 200 || !resp.ok) throw new Error("request error");
+
+  const { results: pokemons }: { results: Pokemon[] } = await resp.json();
+  return pokemons;
+};
+
+export const getPokemonByName = async (name: string): Promise<Pokedex> => {
+  const resp = await fetch(`${POKEAPI_URL}/pokemon/${name}`);
+  if (resp.status !== 200 || !resp.ok) throw new Error("request error");
+
+  const pokemon: Pokedex = await resp.json();
+  return pokemon;
+};
+
+export const getMoveByName = async (moveName: string): Promise<MoveResponse> => {
+  const resp = await fetch(`${POKEAPI_URL}/move/${moveName}`);
+  if (resp.status !== 200 || !resp.ok) throw new Error("request error");
+
+  const move: MoveResponse = await resp.json();
+  return move;
+};

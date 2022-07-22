37e705f2d140fffe4d27d04949e80982cf1456aa
MatiasMonasterio <matias_monasterio@outlook.com>
2022-07-22 10:40:05 -0300

    feat: 🎸 add capitalize function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,8 @@
 export const getOficialArtwork = (image: string): string => {
   return image.replace("pokemon/", "pokemon/other/official-artwork/");
 };
+
+export const capitalize = (value: string): string => {
+  const valueLower = value.toLocaleLowerCase();
+  return valueLower.charAt(0).toUpperCase() + valueLower.slice(1);
+};
